c88990cc3a70d66bb2edd5cb3b94d63f5f46da71
USER <USER@192.168.1.64>
2021-11-11 17:56:52 -0500

    inicio clase 1

---
+++ prueba/src/main/java/prueba/operadores.java
@@ -0,0 +1,22 @@
+package prueba;
+
+public class operadores {
+
+	public static void main(String[] args) {
+		// ctrl+d>eliminar una linea 
+		// matematicos 
+		/*
+		 * +
+		 * -
+		 * +
+		 */
+		  
+	int edad1 = 10;
+	int edad2 = 20;
+	int res = edad1 + edad2;
+	
+	System.out.println("edad1+edad2=" + res);
+
+	}
+
+}
---
+++ prueba/src/main/java/prueba/tipoDeDatos.java
@@ -0,0 +1,14 @@
+package prueba;
+
+public class tipoDeDatos {
+
+	//argumentos o parametros que "viene desde afuera
+	public static void main(String[] args) {
+		// comentario
+		// tipos de datos numericos 
+		// edad > numerico 
+		// byte < short < int < long < double 
+
+	}
+
+}
